2e8aaaf86710e4fe9cf1a3634893ed294b909152
Mahadyar Ravanbakhsh <matt-rb>
2014-11-05 13:59:31 +0100

    Add KNN matching and 10frames subVideo dataset

--- tools/subvideo_10frames_dataset_creat.m
+++ tools/subvideo_10frames_dataset_creat.m
@@ -7,7 +7,7 @@ min_change = 0;
 sub_video_kf = 10;
 itq_data_root = 'features_caffe/';
 train_data_file_name = strcat(itq_data_root,'train_out/itq_train_data_10bit_478caffe_H.mat');
-annotation_file_name = strcat('annotation_my_more.mat');
+annotation_file_name = strcat('annotation_my_more_cell.mat');
 load(train_data_file_name);
 load(annotation_file_name);
 
@@ -17,14 +17,13 @@ sub_video_vectors = [];
 sub_video_extracted_idx = 1;
 
 for video_idx=1:size(mapped,1)
-    query_no = mapped.videoNo(video_idx);
+    query_no = mapped{video_idx,1};
     query_features_file = strcat(itq_data_root,'videos/video',num2str(query_no),'_data.mat');
     load(query_features_file);
     video_query_features = feats;
     fprintf ('%d of %d - Video No : %d  \n', video_idx,size(mapped,1), query_no);
     [ bin_mat ] = test_itq( video_query_features, itq_rot_mat, pca_mapping );
-    bit_changes = claculate_bit_changes( bin_mat );
-    candidate_frames_no = choose_candidate(bit_changes, min_change);
+    candidate_frames_no = choose_keyframes(bin_mat, min_change);
     [ motion_vectors ] = calculate_motion_vectors( feats, candidate_frames_no );
     sub_vid = fix(size(candidate_frames_no,1)/sub_video_kf);
     
---
+++ tools/subvideo_10frames_make_annotation.m
@@ -0,0 +1,42 @@
+
+%load('sub_videos.mat');
+%load('annotation_my_more_cell.mat');
+
+sub_train_set_index = [];
+sub_test_set_index = [];
+flat_dim = 20*size(sub_video_vectors,2);
+
+for i=1:size(annotation_train,1)
+    idx= mapped{strmatch(annotation_train(i,1), mapped(:,2), 'exact'),1};
+    idx= find(sub_video_vectors_index(:,2)==idx);
+    temp= sub_video_vectors_index(idx,:);
+    sub_train_set_index = [ sub_train_set_index ; temp];
+end
+
+for i=1:size(annotation_test,1)
+    idx= mapped{strmatch(annotation_test(i,1), mapped(:,2), 'exact'),1};
+    idx= find(sub_video_vectors_index(:,2)==idx);
+    temp= sub_video_vectors_index(idx,:);
+    sub_test_set_index = [ sub_test_set_index ; temp];
+end
+
+sub_video_test_set_flat= zeros(size(sub_test_set_index,1),flat_dim);
+sub_video_train_set_flat= zeros(size(sub_train_set_index,1),flat_dim);
+
+for i=1:size(sub_test_set_index,1)
+    start_idx=((sub_test_set_index(i,1)-1)*20)+1;
+    end_idx=sub_test_set_index(i,1)*20;
+    temp= sub_video_vectors(start_idx:end_idx,:);
+    temp=reshape(temp,1,flat_dim);
+    sub_video_test_set_flat(i,:)=temp;
+end
+
+for i=1:size(sub_train_set_index,1)
+    start_idx=((sub_train_set_index(i,1)-1)*20)+1;
+    end_idx=sub_train_set_index(i,1)*20;
+    temp= sub_video_vectors(start_idx:end_idx,:);
+    temp=reshape(temp,1,flat_dim);
+    sub_video_train_set_flat(i,:)=temp;
+end
+
+save('sub_video_train_data','sub_test_set_index','sub_train_set_index','sub_video_test_set_flat','sub_video_train_set_flat');

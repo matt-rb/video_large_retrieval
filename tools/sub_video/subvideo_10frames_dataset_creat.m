976678170ec93fb802047498b0a5c9c8c1f2e961
matt-rb <mahdyar.r@hotmail.com>
2014-11-07 17:47:12 +0100

    Correct data normalization bug

--- tools/sub_video/subvideo_10frames_dataset_creat.m
+++ tools/sub_video/subvideo_10frames_dataset_creat.m
@@ -3,13 +3,8 @@
 %------------------ initialization --------------------------------------
 
 clear all;
-min_change = 0;
 sub_video_kf = 10;
-itq_data_root = 'features_caffe/';
-train_data_file_name = strcat(itq_data_root,'train_out/itq_train_data_10bit_411caffe.mat');
-annotation_file_name = strcat('annotation_my_more_cell.mat');
-load(train_data_file_name);
-load(annotation_file_name);
+load_essentials;
 
 
 sub_video_vectors_index=[];
@@ -18,13 +13,15 @@ sub_video_extracted_idx = 1;
 
 for video_idx=1:size(mapped,1)
     query_no = mapped{video_idx,1};
-    query_features_file = strcat(itq_data_root,'videos/video',num2str(query_no),'_data.mat');
+    query_features_file = strcat(features_data_root,'videos/video',num2str(query_no),'_data.mat');
     load(query_features_file);
+    [ feats, ~  ] = normalize_features( feats, mean_data );
     video_query_features = feats;
     fprintf ('%d of %d - Video No : %d  \n', video_idx,size(mapped,1), query_no);
     [ bin_mat ] = test_itq( video_query_features, itq_rot_mat, pca_mapping );
     candidate_frames_no = choose_keyframes(bin_mat, min_change);
     [ motion_vectors ] = calculate_motion_vectors( feats, candidate_frames_no );
+    [ motion_vectors, ~  ] = normalize_features( motion_vectors );
     sub_vid = fix(size(candidate_frames_no,1)/sub_video_kf);
     
     for i=1:sub_vid

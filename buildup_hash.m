6ea8518e64f1a54c92c9a73aa77a5fca3f9cd35e
matt-rb <mahdyar.r@hotmail.com>
2014-11-14 16:20:05 +0100

    Add train data and hash tables for 6samples

--- buildup_hash.m
+++ buildup_hash.m
@@ -6,10 +6,10 @@ load_essentials;
 
 % Collect data and select video key_frames
 [ dataset_frame_table, dataset_motion_table ] = make_dataset_frame_table( dataset_root, pca_mapping, itq_rot_mat, min_change, annotation_train, mapped, mean_data );
-save('features_caffe/dataset_frame_motion_table','dataset_frame_table','dataset_motion_table');
+save(strcat('features_caffe/hash_tables/dataset_frame_motion_table_',num2str(sample_no)),'dataset_frame_table','dataset_motion_table');
 
 % Generate Hash Tables
-load('features_caffe/dataset_frame_motion_table');
+load(strcat('features_caffe/hash_tables/dataset_frame_motion_table_',num2str(sample_no)));
 [ dataset_index_frame ] = make_dataset_index_table( dataset_frame_table );
 [ dataset_index_motion ] = make_dataset_index_table( dataset_motion_table );
 [ dataset_hash_frame ] = make_hash_from_index( dataset_index_frame , dataset_frame_table );
--- load_essentials.m
+++ load_essentials.m
@@ -2,12 +2,13 @@
 
 pca_size = 10;
 min_change = 0;
+sample_no = 6;
 
 
 features_data_root = 'features_caffe/';
 dataset_root = 'features_caffe/videos/';
-dataset_hash_table_file= 'features_caffe/hash_tables_sub/dataset_hash_table_1';
-train_data_file_name = strcat(features_data_root,'train_out/itq_train_data_',num2str(pca_size),'bit_411caffe_1.mat');
+dataset_hash_table_file= strcat('features_caffe/hash_tables/dataset_hash_table_',num2str(sample_no));
+train_data_file_name = strcat(features_data_root,'train_out/itq_train_data_',num2str(pca_size),'bit_411caffe_',num2str(sample_no),'.mat');
 annotation_file_name = strcat('annotation_my_more_cell.mat');
 
 
